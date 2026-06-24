6571b00ec40d7037b2218b2653677702ea47ad79
surayaafroze63 <surayaafroze63@gmail.com>
2026-06-24 08:56:37 +0600

    Fallback to getSession token if jwt endpoint returns 404

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -13,8 +13,17 @@ export const getValidToken = async () => {
   let token = localStorage.getItem('token');
   if (!token) {
     try {
-      const { data } = await authClient.jwt();
-      token = data?.jwt || '';
+      let { data } = await authClient.jwt().catch(() => ({ data: null }));
+      if (data?.jwt) {
+        token = data.jwt;
+      } else {
+        // Fallback to getting the session token which is also a JWT
+        const sessionRes = await authClient.getSession();
+        if (sessionRes?.data?.session?.token) {
+          token = sessionRes.data.session.token;
+        }
+      }
+      
       if (token) {
         localStorage.setItem('token', token);
       }
