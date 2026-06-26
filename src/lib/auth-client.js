8e07339398813e07e65872d2d904a832a40563eb
surayaafroze63 <surayaafroze63@gmail.com>
2026-06-26 17:45:10 +0600

    Force baseURL to origin

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,7 +2,7 @@ import { jwtClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || (typeof window !== 'undefined' ? window.location.origin : "https://recipe-hub-gamma-nine.vercel.app"),
+    baseURL: typeof window !== 'undefined' ? window.location.origin : (process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000"),
     plugins: [jwtClient()]
 });
 
